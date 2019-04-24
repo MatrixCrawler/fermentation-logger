4cb16db8866c2d258ce4cb3a58e5c45bf9106c72
Johannes Brunswicker <johannes.brunswicker@gmail.com>
2019-04-24 11:37:32 +0200

    add argument parser

--- sensor.py
+++ sensor.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
-
+import argparse
 import time
 from datetime import datetime
 
 import RPi.GPIO as GPIO
 from influxdb import InfluxDBClient
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-bubble_counter = 0
+options = {}
 
 
-def my_callback(channel):
-    global bubble_counter
-    bubble_counter += 1
-    print(str(channel) + ", Bubbles: " + str(bubble_counter))
-    args = dict(username="USER", password="PASSWORD", host="localhost",
-                database="DBName")
+def _write_measurement(channel):
+    global options
+    args = dict(username=options.influx_username, password=options.influx_password, host=options.influx_hostname,
+                database=options.influx_database)
     influx = InfluxDBClient(**args)
     data_point = dict(
-        measurement="measurement",
+        measurement=options.influx_measurement,
         time=str(datetime.utcnow()),
         fields=dict(
             bubble=1
@@ -30,11 +25,54 @@ def my_callback(channel):
     influx.close()
 
 
-GPIO.add_event_detect(14, GPIO.RISING, callback=my_callback, bouncetime=1000)
-try:
-    while True:
-        # warten!
-        time.sleep(1)
-except KeyboardInterrupt:
-    GPIO.cleanup()
-    print("\nI am out!")
+def _setup_gpio(channel):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(channel, GPIO.RISING, callback=_write_measurement, bouncetime=1000)
+
+
+def _read_parser():
+    global options
+    parser = argparse.ArgumentParser(conflict_handler="resolve",
+                                     description="This will track the IR Sensor on specific channel and post events in InfluxDB")
+    parser.add_argument("-H", "--host", type=str, dest="influx_hostname", help="The hostname of the InfluxDB",
+                        default="localhost")
+    parser.add_argument("-U", "--username", type=str, dest="influx_username", help="The username for the InfluxDB",
+                        required=True)
+    parser.add_argument("-P", "--password", type=str, dest="influx_password", help="The password for the InfluxDB",
+                        required=True)
+    parser.add_argument("-D", "--database", type=str, dest="influx_database", help="The database in theInfluxDB",
+                        required=True)
+    parser.add_argument("-M", "--measurement", type=str, dest="influx_measurement",
+                        help="The measurement in theInfluxDB Database", required=True)
+    parser.add_argument("-C", "--channel", type=int, dest="channel",
+                        help="The channel on the GPIO Board", required=True)
+    options = parser.parse_args()
+    if not options.influx_username:
+        parser.error("Username not given")
+    if not options.influx_password:
+        parser.error("Password not given")
+    if not options.influx_database:
+        parser.error("Database not given")
+    if not options.influx_measurement:
+        parser.error("Measurement not given")
+    if not options.channel:
+        parser.error("Channel not given")
+
+
+def main():
+    global options
+    _read_parser()
+    _setup_gpio(options.channel)
+    try:
+        while True:
+            # warten!
+            time.sleep(1)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print("\nI am out!")
+
+
+"""main method"""
+if __name__ == "__main__":
+    main()
